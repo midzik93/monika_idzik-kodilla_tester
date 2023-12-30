9fc13ec4c9a8dc1256706d7a66d90f3cab38a169
Monika Idzik <midzik93@gmail.com>
2023-12-30 22:56:01 +0100

    zadanie 2.6

---
+++ kodilla-intro/src/main/java/Application.java
@@ -0,0 +1,27 @@
+public class Application {
+    double height;
+    int age;
+    String name;
+
+    public static void main (String[] args){
+        Application application = new Application ("Adam", 25, 178.5);
+        application.checkUserApplication();
+    }
+
+    public Application (String name, int age, double height) {
+        this.height = height;
+        this.age = age;
+        this.name = name;
+    }
+
+    public void checkUserApplication() {
+        if (this.name != null) {
+            if (this.age > 30 && this.height > 160) {
+                System.out.println("User is older than 30 and taller than 160cm");
+            } else {
+                System.out.println("User is 30 (or younger) or 160cm (or shorter)");
+            }
+        }
+
+    }
+}
