28e48c51f34a994f2b90d27425716007e9d21d84
Monika Idzik <midzik93@gmail.com>
2024-01-02 13:16:46 +0100

    zadanie 2.3

--- kodilla-intro/src/main/java/FirstClass.java
+++ kodilla-intro/src/main/java/FirstClass.java
@@ -1,32 +1,32 @@
 import java.awt.*;
 
 public class FirstClass {
-    public static void main(String[] args){//System.out.println(notebook);   // przekazywanie całego obiektu notebook do System.out.println
-        Notebook notebook = new Notebook (600, 3000, 2021);
-        System.out.println("notebook.weight: " +notebook.weight + " notebook.price: " + notebook.price);// wywołujemy zmienną weight z obiektu notebook
-notebook.checkPrice();  //weryfikacja dla wszystkich
+    public static void main(String[] args) {//System.out.println(notebook);   // przekazywanie całego obiektu notebook do System.out.println
+        Notebook notebook = new Notebook(600, 3000, 2021);
+        System.out.println("notebook.weight: " + notebook.weight + " notebook.price: " + notebook.price);// wywołujemy zmienną weight z obiektu notebook
+        notebook.checkPrice();  //weryfikacja dla wszystkich
         notebook.checkWeight();
         notebook.checkYearAndPrice();
 
-        Notebook heavyNotebook = new Notebook (2500, 5000, 2023);
-        System.out.println("heavyNotebook.weight: " +heavyNotebook.weight + " heavyNotebook.price: "+ heavyNotebook.price);
-heavyNotebook.checkPrice();
+        Notebook heavyNotebook = new Notebook(2500, 5000, 2023);
+        System.out.println("heavyNotebook.weight: " + heavyNotebook.weight + " heavyNotebook.price: " + heavyNotebook.price);
+        heavyNotebook.checkPrice();
         heavyNotebook.checkWeight();
         heavyNotebook.checkYearAndPrice();
 
-        Notebook oldNotebook = new Notebook (2000, 1500, 2018);
+        Notebook oldNotebook = new Notebook(2000, 1500, 2018);
         System.out.println("oldNotebook.weight: " + oldNotebook.weight + " oldNotebook.price: " + oldNotebook.price);
-oldNotebook.checkPrice();
+        oldNotebook.checkPrice();
         oldNotebook.checkWeight();
         oldNotebook.checkYearAndPrice();
 
-        Notebook cheapNotebook = new Notebook (2000, 1000, 2020);
+        Notebook cheapNotebook = new Notebook(2000, 1000, 2020);
         System.out.println("cheapNotebook.weight: " + cheapNotebook.weight + " cheapNotebook.price: " + cheapNotebook.price);
         cheapNotebook.checkPrice();
         cheapNotebook.checkWeight();
         cheapNotebook.checkYearAndPrice();
 
-        Notebook expensiveNotebook = new Notebook (2000, 8000, 2023);
+        Notebook expensiveNotebook = new Notebook(2000, 8000, 2023);
         System.out.println("expensiveNotebook.weight: " + expensiveNotebook.weight + " expensiveNotebook.price: " + expensiveNotebook.price);
         expensiveNotebook.checkPrice();
         expensiveNotebook.checkWeight();
@@ -38,5 +38,5 @@ oldNotebook.checkPrice();
 //    System.out.println("This notebook is expensive");
 //}
     }
-    }
+}
 
