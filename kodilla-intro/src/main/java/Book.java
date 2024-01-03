35510ff96925a271d7a252f2e5de3e5a93d2679f
Monika Idzik <midzik93@gmail.com>
2024-01-03 22:03:40 +0100

    zadanie 2.7

--- kodilla-intro/src/main/java/Book.java
+++ kodilla-intro/src/main/java/Book.java
@@ -2,14 +2,15 @@ public class Book {
     private String author;
     private String title;
 
-        public static Book of(String author, String title) { //tworzy i zwraca obiekt of
-            Book book = new Book();
-            book.author = author;
-            book.title = title;
-            return book;
-        }
-public static void main(String[] args){
-        Book book = Book.of ("Isaac Asimov", "The Galaxy");
-        System.out.println( "Autorem książki " + book.title + " jest " + book.author);
-}
+    public static Book of(String author, String title) { //tworzy i zwraca obiekt of
+        Book book = new Book();
+        book.author = author;
+        book.title = title;
+        return book;
     }
+
+    public static void main(String[] args) {
+        Book book = Book.of("Isaac Asimov", "The Galaxy");
+        System.out.println("Autorem książki " + book.title + " jest " + book.author);
+    }
+}
