2b940e1c2f4d50d37864751aaf48d1bc8e76e983
Monika Idzik <midzik93@gmail.com>
2024-01-14 15:11:04 +0100

    zadanie 3.5

---
+++ kodilla-basic-tests/src/main/java/com/kodilla/basic_assertion/Application.java
@@ -0,0 +1,40 @@
+package com.kodilla.basic_assertion;
+
+public class Application {
+    public static void main(String[] args){
+
+        Calculator calculator=new Calculator();
+        int a=5;
+        int b=8;
+        int sumResult=calculator.sum(a,b);
+
+        //by sprawdzić czy wynik jest poprawny używamy metody assertEquals
+        //z klasy ResultChecker który zwraca wartość boolean
+        //wartość boolean można przypisać do zmiennej
+
+        boolean correct=ResultChecker.assertEquals(13,sumResult);
+        if (correct) {
+            System.out.println("Metoda działa poprawnie dla tego zestawu danych");
+        } else {
+            System.out.println("Metoda działa niepoprawnie dla tego zestawu danych");
+        }
+
+        int subResult= calculator.subtract(a,b);
+        boolean correct1=ResultChecker.assertEquals(3,subResult);
+        if (correct1){
+            System.out.println("Metoda działa poprawnie dla tego zestawu danych");
+        } else {
+            System.out.println("Metoda działa niepoprawnie dla tego zestawu danych");
+
+        }
+
+        int squareResult= calculator.square(a);
+        boolean correct3=ResultChecker.assertEquals(25,squareResult);
+        if(correct3){
+            System.out.println("Metoda działa poprawnie dla tego zestawu danych");
+        } else {
+            System.out.println("Metoda działa niepoprawnie dla tego zestawu danych");
+
+        }
+    }
+}
---
+++ kodilla-basic-tests/src/main/java/com/kodilla/basic_assertion/Calculator.java
@@ -0,0 +1,18 @@
+package com.kodilla.basic_assertion;
+
+public class Calculator {
+    private int a;
+    private int b;
+
+    public int sum(int a, int b) {
+        return a + b;
+    }
+
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public int square(int a) {
+        return a * a;
+    }
+}
