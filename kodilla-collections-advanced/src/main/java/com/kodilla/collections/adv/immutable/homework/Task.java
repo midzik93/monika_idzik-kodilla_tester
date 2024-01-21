0723531f08b45c5f53d286524cccfa94bd8af95f
Monika Idzik <midzik93@gmail.com>
2024-01-21 19:17:56 +0100

    zadanie 5.3

---
+++ kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/immutable/homework/Task.java
@@ -0,0 +1,19 @@
+package com.kodilla.collections.adv.immutable.homework;
+
+public class Task {
+    protected String title;
+    protected int duration;
+
+    public Task(String title, int duration) {
+        this.title = title;
+        this.duration=duration;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+}
