6d238c24ddd9cb9c74f224acfba777d0bf380980
Monika Idzik <midzik93@gmail.com>
2024-03-09 09:55:21 +0100

    zadanie 9.2 poprawka

--- kodilla-spring-basic/src/main/java/com/kodilla/spring/basic/dependency_injection/homework/NotificationService.java
+++ kodilla-spring-basic/src/main/java/com/kodilla/spring/basic/dependency_injection/homework/NotificationService.java
@@ -6,11 +6,4 @@ public interface NotificationService {
     public void fail(String address);
 }
 
-//        public void success(String address) {
-//            System.out.println("Package delivered to: " + address);
-//        }
-//
-//        public void fail(String address) {
-//            System.out.println("Package not delivered to: " + address);
-//        }
 
