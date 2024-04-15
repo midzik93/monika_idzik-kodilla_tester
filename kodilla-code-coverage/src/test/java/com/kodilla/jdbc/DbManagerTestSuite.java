e9ccd0845d83a810d1c690ae53f182327f5042a6
midzik3 <midzik93@gmail.com>
2024-04-15 22:38:18 +0200

    Zadanie 15.3

--- kodilla-code-coverage/src/test/java/com/kodilla/jdbc/DbManagerTestSuite.java
+++ kodilla-code-coverage/src/test/java/com/kodilla/jdbc/DbManagerTestSuite.java
@@ -7,7 +7,6 @@ import java.util.ArrayList;
 import java.util.List;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import java.sql.SQLException;
 
